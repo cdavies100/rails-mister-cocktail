3bf877af46dc0cece579f3e699548b63c55b8309
Chiara Davies <chiaradavies01@outlook.com>
2017-08-17 13:06:59 +0100

    changed the dose column name from amount to description because rake

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,5 +2,7 @@ class Ingredient < ApplicationRecord
   has_many :doses
   has_many :cocktails, through: :doses
 
+  validates :name, presence: true, uniqueness: true
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817114441) do
+ActiveRecord::Schema.define(version: 20170817120451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20170817114441) do
   end
 
   create_table "doses", force: :cascade do |t|
-    t.string   "amount"
+    t.string   "description"
     t.integer  "cocktail_id"
     t.integer  "ingredient_id"
     t.datetime "created_at",    null: false
